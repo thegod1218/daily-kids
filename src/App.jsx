40944646e362c502f768c4b94f62d9f9687e64cf
thegod1218 <rebirth0109@gmail.com>
2026-06-02 11:24:32 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ParentDashboard from './pages/parent/ParentDashboard'
+import HomePage from './pages/child/HomePage'
+import MissionsPage from './pages/child/MissionsPage'
+import MoneyPage from './pages/child/MoneyPage'
+import BooksPage from './pages/child/BooksPage'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ParentDashboard />
+      <Routes>
+        <Route path="/parent/dashboard" element={<ParentDashboard />} />
+        <Route path="/child/home"        element={<HomePage />} />
+        <Route path="/child/missions"    element={<MissionsPage />} />
+        <Route path="/child/money"       element={<MoneyPage />} />
+        <Route path="/child/books"       element={<BooksPage />} />
+        <Route path="*" element={<Navigate to="/parent/dashboard" replace />} />
+      </Routes>
     </BrowserRouter>
   )
 }
