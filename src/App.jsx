017d5542403aa6034b1e1c56f83397dbffebe887
thegod1218 <rebirth0109@gmail.com>
2026-06-02 11:10:11 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { useEffect } from "react";
 import ParentDashboard from "./pages/parent/ParentDashboard";
 import { useAppStore } from "./store";
-import { useEffect } from "react";
 
 export default function App() {
   const { checkDailyReset } = useAppStore();
-  
+
   useEffect(() => {
     checkDailyReset();
   }, []);
